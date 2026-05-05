9e9d53379d44057cd6c7b1868d3c3efe88295e60
HemanthKumarBurada <hemanthburada2003@gmail.com>
2026-05-05 11:59:35 +0530

    fix: add vercel domain to cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,7 +34,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://smartscreen-eight.vercel.app"  # ✅ ADD THIS
     ],
     allow_credentials=True,
     allow_methods=["*"],
